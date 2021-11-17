a9ff035f587c30a264e1d8630e85eb6d080cc4d2
Shubham <shubh.chavhan00@gmail.com>
2021-11-17 22:15:17 +0530

    created Button component and added styling

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Button = styled.a`
+  border: none;
+  outline: none;
+  margin-right: 20px;
+  padding: 8px 12px;
+  border-radius: 3px;
+  font-size: 1rem;
+  color: currentColor;
+  text-decoration: none;
+`;
---
+++ src/components/ButtonContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "./Button";
+
+const ButtonContainer = () => {
+  return (
+    <Wrapper>
+      <Button href="mailto:shubh.chavhan99@gmail.com" className="toggle">
+        Email me
+      </Button>
+      <Button className="toggle">Resume</Button>
+    </Wrapper>
+  );
+};
+
+export default ButtonContainer;
+
+const Wrapper = styled.div``;
