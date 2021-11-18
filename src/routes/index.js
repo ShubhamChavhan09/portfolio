fcce5875c3420f94668025e902f38b9987f2a807
Shubham <shubh.chavhan00@gmail.com>
2021-11-18 15:37:24 +0530

    added Projectdetail to react-router

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Skills from "../pages/Skills";
 import Projects from "../pages/Projects";
 import NotFound from "../pages/404";
 import Home from "../pages/Home";
+import ProjectDetail from "../pages/ProjectDetail";
 
 const Pages = () => {
   return (
@@ -14,6 +15,7 @@ const Pages = () => {
         <Route path="/about" exact element={<About />} />
         <Route path="/skills" exact element={<Skills />} />
         <Route path="/projects" exact element={<Projects />} />
+        <Route path="/projects/:id" exact element={<ProjectDetail />} />
         <Route component={NotFound} />
       </Routes>
     </div>
