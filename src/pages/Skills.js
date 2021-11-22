fd81fe861abba25191ceace0d53c16adf81b6c97
Shubham <shubh.chavhan00@gmail.com>
2021-11-22 12:56:49 +0530

    Changed layout and added hover effect to show title of skills and made mobile responsive

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Tooltip } from "react-tippy";
 import styled from "styled-components";
 import { techSkills } from "../data/skills";
 
@@ -9,8 +10,10 @@ const Skills = () => {
       <Container>
         {techSkills.map((tech) => (
           <TechWrapper key={tech.id}>
-            <Icon>{tech.icon}</Icon>
-            <Text className="paragraph">{tech.name}</Text>
+            <Tooltip title={tech.name} position="bottom" animation="fade">
+              <Icon>{tech.icon}</Icon>
+              {/* <Text className="paragraph">{tech.name}</Text>  */}
+            </Tooltip>
           </TechWrapper>
         ))}
       </Container>
@@ -26,27 +29,34 @@ const Wrapper = styled.div`
 
 const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 1rem;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 3rem;
+  margin-top: 2rem;
+  @media (max-width: 414px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
 `;
 
 const TechWrapper = styled.div`
   display: flex;
-  justify-content: flex-start;
+  justify-content: center;
   align-items: center;
-  padding: 0 30px;
-  border: 0.01rem solid #333;
   border-radius: 8px;
-  letter-spacing: 1.5px;
-  
+  margin-right: 14rem;
+  @media (max-width: 1024px) {
+    margin-right: 6rem;
+  }
+  @media (max-width: 414px) {
+    margin-right: 1rem;
   }
 `;
 
 const Icon = styled.span`
   font-size: 4rem;
-`;
-
-const Text = styled.span`
-  margin: 0 auto;
-  font-size: 1.2rem;
+  @media (max-width: 414px) {
+    font-size: 2.5rem;
+  }
+  @media (max-width: 360px) {
+    font-size: 2rem;
+  }
 `;
