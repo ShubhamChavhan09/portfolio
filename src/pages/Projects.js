bc4e019ae65651bbd9ed924c7cee92b403ad6073
Shubham <shubh.chavhan00@gmail.com>
2021-11-28 07:52:14 +0530

    Displayed project details in each components

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,67 +1,103 @@
 import React from "react";
 import styled from "styled-components";
 import { projectData } from "../data/projects";
-import { Link } from "react-router-dom";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 const Projects = () => {
   return (
-    <div>
-      {projectData.map((project) => (
-        <Wrapper key={project.id}>
-          <img src={project.thumb} alt="" />
-
-          <Link to={`/projects/${project.id}`}>
-            <Overlay className="overlay">
-              <h1> {project.title}</h1>
-            </Overlay>
-          </Link>
-        </Wrapper>
-      ))}
-    </div>
+    <Wrapper>
+      <SubHeading>Collection of personal web dev projects</SubHeading>
+      <Container>
+        {projectData.map((project) => (
+          <Card key={project.id}>
+            <h2>{project.title}</h2>
+            <p>{project.detail}</p>
+            <Tech>
+              {project.tech.map((item) => (
+                <p> {item}</p>
+              ))}
+            </Tech>
+            <Links>
+              <Link href={project.github} target="_blank" rel="noreferrer">
+                GitHub
+                <RiExternalLinkLine />
+              </Link>
+              <Link href={project.appLink} target="_blank" rel="noreferrer">
+                Live App
+                <RiExternalLinkLine />
+              </Link>
+            </Links>
+          </Card>
+        ))}
+      </Container>
+    </Wrapper>
   );
 };
 
-export default Projects;
-
 const Wrapper = styled.div`
-  height: 200px;
-  overflow: hidden;
-  border-radius: 12px;
-  position: relative;
-  border: 1px solid #333;
-  margin: 40px 0;
-  &:hover .overlay {
-    transform: translateY(0);
-    opacity: 1;
-  }
-
-  img {
-    object-fit: cover;
-    width: 2000%;
-  }
-
-  @media (max-width: 700px) {
-    height: 120px;
-    margin: 20px 0;
-  }
-`;
-
-const Overlay = styled.div`
-  z-index: 1;
   display: flex;
-  justify-content: flex-start;
-  align-items: end;
-  position: absolute;
-  inset: -5px;
-  background: rgba(0, 0, 0, 0.8);
-  opacity: 0;
-  transition: all 0.5s ease-in-out;
-  transform: translateY(3%);
-  color: white;
-  cursor: pointer;
+  flex-direction: column;
+  margin-bottom: 40px;
+`;
 
-  h1 {
-    font-size: 3rem;
-    margin-left: 2rem;
+const SubHeading = styled.p`
+  color: ${({ theme }) => theme.text};
+  font-size: 1.5rem;
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-direction;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid;
+  border-radius: 15px;
+  margin: 1rem 0;
+  padding: 1rem;
+
+  p {
+    font-weight: 300;
+  }
+
+  h2 {
+    font-weight: 400;
+    margin: 0;
   }
 `;
+
+const Tech = styled.div`
+  display: flex;
+  justify-content: content;
+  align-items: center;
+  flex-wrap: wrap;
+  p {
+    font-size: 0.9rem;
+    margin: 0.5rem;
+    background: ${({ theme }) => theme.toggle};
+    padding: 0.5rem;
+    border-radius: 8px;
+  }
+`;
+
+const Links = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+const Link = styled.a`
+  text-decoration: none;
+  color: #666;
+  margin-right: 1rem;
+  :hover {
+    color: currentColor;
+    transition: all 0.5s ease;
+  }
+`;
+
+export default Projects;
