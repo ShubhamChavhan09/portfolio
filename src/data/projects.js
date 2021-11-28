36e280df16b15b91a34214483e77af843e3c6f39
Shubham <shubh.chavhan00@gmail.com>
2021-11-28 07:50:25 +0530

    Deleted project images and added link data for liveapp and Github

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,33 +1,38 @@
-import CookBook from "./images/CookBook.jpg";
-import Expense from "./images/Expense.jpg";
-import fq from "./images/fq.jpg";
-import GitUser from "./images/GitUser.jpg";
-import Breaking from "./images/breakingbad.jpg";
-
 export const projectData = [
   {
     id: 0,
-    title: "Meal Recipe",
-    thumb: CookBook,
+    title: "Cook Book",
+    detail:
+      "Provide recipes to the users based on food names, categories available with the API set recipes as favourites.",
+    tech: ["React", "MealDB API", "Styled-Components", "React-Router"],
+    github: "https://github.com/ShubhamChavhan09/meal-recipes-react",
+    appLink: "https://meal-recipes.netlify.app/",
   },
   {
     id: 1,
-    title: "Github username search",
-    thumb: GitUser,
+    title: "Github users",
+    detail:
+      "This is a solution to the GitHub user search app challenge on Frontend Mentor, which helps to search users and view their profiles on Github.",
+    tech: ["React", "Github API", "Styled-Components", "React-Router"],
+    github: "https://github.com/ShubhamChavhan09/github-user-search-app",
+    appLink: "https://reactjs-github-user-search.netlify.app/",
   },
   {
     id: 2,
-    title: "Breaking Bad",
-    thumb: Breaking,
+    title: "Breaking Bad Character",
+    detail:
+      "Shows every character from Breaking Bad with their personal details with ability to search by name.",
+    tech: ["React", "Styled-Components"],
+    github: "https://github.com/ShubhamChavhan09/breaking-bad-characters",
+    appLink: "https://breakingbad-character-cards.netlify.app/",
   },
   {
     id: 3,
     title: "Expense Tracker",
-    thumb: Expense,
-  },
-  {
-    id: 4,
-    title: " Accordion ",
-    thumb: fq,
+    detail:
+      "A monthly expense tracker app automates the process of recording transactions, totaling expenses by category and tracking progress toward goals. ",
+    tech: ["React"],
+    github: "https://github.com/ShubhamChavhan09/expense-tracker",
+    appLink: "https://react-js-expense-tracker.netlify.app/",
   },
 ];
