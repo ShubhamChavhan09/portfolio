574b7b97625040384dc0b2871617cd02a4992e65
Shubham <shubh.chavhan00@gmail.com>
2021-11-28 07:41:24 +0530

    Added Resume to button a tag

--- src/components/ButtonContainer.js
+++ src/components/ButtonContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Button } from "./Button";
+import Resume from "../assets/Shubham_Chavhan-resume.pdf";
 
 const ButtonContainer = () => {
   return (
@@ -8,7 +9,9 @@ const ButtonContainer = () => {
       <Button href="mailto:shubh.chavhan99@gmail.com" className="toggle">
         shubh.chavhan99@gmail.com
       </Button>
-      <Button className="toggle">Resume</Button>
+      <Button href={Resume} target="_blank" rel="noreferrer" className="toggle">
+        Resume
+      </Button>
     </Wrapper>
   );
 };
